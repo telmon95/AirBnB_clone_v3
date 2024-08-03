da2bea0c39435e00e84ef190d487a7c3ef427a21
telmon95 <telmonm@gmail.com>
2024-08-03 02:47:28 +0200

    updated code

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,23 +1,34 @@
+#!/usr/bin/python3
+'''
+    flask with general routes
+    routes:
+        /status:    display "status":"OK"
+        /stats:     dispaly total for all classes
+'''
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
 
-@app_views.route('/status', methods=['GET'])
-def status():
-    return jsonify({"status": "OK"})
 
-@app_views.route('/stats', methods=['GET'])
-def get_stats():
-    """Retrieves the number of each objects by type"""
-    classes = {
-        "amenities": "Amenity",
-        "cities": "City",
-        "places": "Place",
-        "reviews": "Review",
-        "states": "State",
-        "users": "User"
+@app_views.route("/status")
+def status():
+    '''
+        return JSON of OK status
+    '''
+    return jsonify({'status': 'OK'})
+
+
+@app_views.route("/stats")
+def storage_counts():
+    '''
+        return counts of all classes in storage
+    '''
+    cls_counts = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
     }
-    stats = {}
-    for key, value in classes.items():
-        stats[key] = storage.count(value)
-    return jsonify(stats)
+    return jsonify(cls_counts)
